60462157c64f968b842944de2739298b2b765403
Clea Monique Sacriz <clea.sacriz@deped.gov.ph>
2025-12-23 14:05:28 +0800

    dsad

--- src/Login.jsx
+++ src/Login.jsx
@@ -206,7 +206,7 @@ const Login = () => {
                         download="InsightEd.apk"
                         style={{
                             display: 'inline-block',
-                            backgroundColor: '#22c55e', // Green color
+                            backgroundColor: '#00ff11ff',
                             color: 'white',
                             padding: '10px 20px',
                             borderRadius: '12px',
